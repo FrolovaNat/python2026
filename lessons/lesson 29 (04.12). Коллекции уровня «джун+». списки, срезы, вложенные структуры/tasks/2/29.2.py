23ab653da0fb4872daf23ea3fd3adae670069265
FrolovaNat <kuhlstr@mail.ru>
2025-12-04 21:19:34 +0500

    Add files via upload

---
+++ lessons/lesson 29 (04.12). Коллекции уровня «джун+». списки, срезы, вложенные структуры/tasks/2/29.2.py
@@ -0,0 +1,10 @@
+summ = [88, 92, 101, 109, 115, 118, 120, 117, 110, 100, 95]
+print(summ)
+rang = summ[0:5]
+print(rang)
+rang1 = summ[2:8]
+print(rang1)
+#rang2 = summ[::2]
+#print(rang2) не помню как выводить каждый третий
+rang3 = summ[2:9]
+print(rang3)
