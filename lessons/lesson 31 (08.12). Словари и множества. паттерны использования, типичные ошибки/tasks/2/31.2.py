6575b29c960394964348d70c77a09c49a79d0897
FrolovaNat <kuhlstr@mail.ru>
2025-12-08 20:53:47 +0500

    Create 31.2.py

---
+++ lessons/lesson 31 (08.12). Словари и множества. паттерны использования, типичные ошибки/tasks/2/31.2.py
@@ -0,0 +1,11 @@
+    
+pledges = [('anna', 300), ('pavel', 150), ('anna', 200), ('dasha', 400)]
+finished = ['pavel']
+
+donations = {}
+for name,count in pledges:
+    donations[name] = donations.get(name,0) + count
+for y in finished:
+    removed = donations.pop(y, 'вернул деньги')
+    print(f"{y}: {removed}")
+print(donations)
