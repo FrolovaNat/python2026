d787c26bda9291e314f6b641d38e0aba98da1e5e
FrolovaNat <kuhlstr@mail.ru>
2025-12-18 20:17:59 +0500

    Add files via upload

---
+++ lessons/lesson 37 (18.12). Практическая работа с функциями/tasks/2/37.2.py
@@ -0,0 +1,15 @@
+#1
+def delivery_price(city, weight_kg, urgent):
+    if urgent=True:
+        return 500 + 30 * weight_kg
+    else:
+        return 300 + 20 * weight_kg
+print(delivery_price(city, weight_kg, urgent)
+#2
+def delivery_price(text, price):
+    return delivery_price 
+print(delivery_price(text, price))
+print(delivery_price(text = "яблоко", price = "100"))
+ 
+#3
+
