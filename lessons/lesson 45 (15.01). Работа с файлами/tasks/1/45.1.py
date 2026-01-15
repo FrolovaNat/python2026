79285ce5e4cbccb689ad99180890660473823247
FrolovaNat <kuhlstr@mail.ru>
2026-01-15 20:52:58 +0500

    Delete lessons/lesson 45 (15.01). Работа с файлами/tasks/1/45.1.py

--- lessons/lesson 45 (15.01). Работа с файлами/tasks/1/45.1.py
+++
@@ -1,7 +0,0 @@
-def print_attendees(path: str):
-     try:
-        with open(path.txt, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-     except FileNotFoundError:
-         print("Файл не найден.")
